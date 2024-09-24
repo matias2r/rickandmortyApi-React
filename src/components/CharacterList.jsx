269946fed2aaf1eca4725d33f3d7d9fb99a8f8df
matias2r <matias.espinozamo@gmail.com>
2024-09-23 23:39:03 -0300

    add: Logo github con enlace a repositorio

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -69,6 +69,19 @@ const CharacterList = () => {
   ))}
 </div>
 
+      <div className="flex flex-col justify-center items-center mt-10 mb-20 font-semibold">
+         <a href="https://github.com/matias2r/rickandmortyApi-React" target="_blank" rel="noopener noreferrer">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
+            className="filter invert w-5 mb-2" 
+            alt="GitHub Icon" 
+          />
+        </a>
+        <span className='text-zinc-400'>
+          ❮❯ by <a href="https://github.com/matias2r" target="_blank" rel="noopener noreferrer" className='text-white'>Matias Espinoza</a> 2024
+        </span>
+      </div>
+
     </>
   );
 };
